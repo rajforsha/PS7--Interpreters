910e7124f6fc14dc3fb8cbcb85b3a6000ee7e7d9
Shashi Raj <shraj@groupon.com>
2020-06-03 20:45:23 +0530

    refactor method

--- src/Interpreter.py
+++ src/Interpreter.py
@@ -3,6 +3,7 @@ class InterPreter:
     def __init__(self):
         self.vertices = [] #list containing languages and interpreter
         self.edges = {} #adjency matrix of edges linking interpreters to languages
+        self.interpreters = [] #need to maintain since vertices contain both the langauge and interpreter
 
     def addLanguagesOrInterpreters(self, val):
         val = val.strip()
@@ -11,12 +12,20 @@ class InterPreter:
 
 
     def linkInterpretersToLangauages(self, interpreter, language):
+        #undirected graph, hence adding to both the ends
         interpreter = interpreter.strip()
         language = language.strip()
+
         if self.edges.get(interpreter) is None:
             self.edges[interpreter]=[language]
         else:
             self.edges[interpreter].append(language)
+
+        if self.edges.get(language) is None:
+            self.edges[language] = [interpreter]
+        else:
+            self.edges[language].append(interpreter)
+
 
     def readInputFile(self, filepath):
         with open(filepath) as f:
@@ -24,41 +33,42 @@ class InterPreter:
             for line in lines:
                 values = line.strip('\n').split('/')
                 name = values[0]
+                self.interpreters.append(name.strip())
                 self.addLanguagesOrInterpreters(name)
                 for i in range(1, len(values)):
                     self.linkInterpretersToLangauages(name, values[i])
                     self.addLanguagesOrInterpreters(values[i])
 
+
+    def getAllLanguages(self):
+        s1 = set(self.vertices)
+        s2 = set(self.interpreters)
+        return s1-s2
+
+    def printGraph(self):
+        for key,val in self.edges.items():
+            print(key, val)
+
     def showAll(self):
         print('--------Function showAll--------')
-        print('Total no. of candidates: ', len(self.edges.keys()))
-        s1 = set(self.vertices)
-        s2 = set(self.edges.keys())
 
-        print('Total no. of languages: ', len(s1-s2))
+        print('Total no. of candidates: ', len(self.interpreters))
+        print('Total no. of languages: ', len(self.getAllLanguages()))
 
         print('List of candidates:')
-        for candidate in s2:
+        for candidate in self.interpreters:
             print(candidate)
-
-        languages = s1-s2
 
         print('List of languages:')
 
-        for language in languages:
+        for language in self.getAllLanguages():
             print(language)
 
-
-    def print(self):
-        print('-----------vertices are -----------')
-        print(self.vertices)
-        print('--------------edges are ------------')
-        print(self.edges)
-
-
-
+    # def displayHireList(self):
 
 if __name__ == '__main__':
     ob = InterPreter()
     ob.readInputFile('../resources/Sample inputPS7.txt')
     ob.showAll()
+    ob.printGraph()
+    # ob.displayHireList()

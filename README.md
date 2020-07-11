e134b30e305e6a6b871e1c5aaa85fb9d9efd6241
Shashi Raj <shraj@groupon.com>
2020-07-11 19:54:58 +0530

    removed print merthod

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # PS7--Interpreters-
+
+
+#backup print method
+
+    """def printGraph(self):
+        output_result_list = []
+        output_result_list.append('--------Function printGraph--------')
+
+        print('\t\t', end = '')
+        for row in range(self.vertices):
+            print(self.mapping.get(row), ' ', end ='')
+        print()
+
+        for row in range(self.vertices):
+            print(self.mapping.get(row), ' ', end = '')
+            for column in range(self.vertices):
+                print(self.graph[row][column], '\t', end='')
+            print()
+
+        # for row in range(self.vertices):
+        #     output_result_list.append(str(self.edges[row]))
+
+        # write to output file
+        self.utils.writeToOutputFile(output_result_list)"""
+        
--- src/Interpreter.py
+++ src/Interpreter.py
@@ -73,27 +73,6 @@ class Interpreter:
                     index += 1
                 self.__addEdges(self.mapping.get(name), self.mapping.get(value)) #populate matrix
 
-    """def printGraph(self):
-        output_result_list = []
-        output_result_list.append('--------Function printGraph--------')
-
-        print('\t\t', end = '')
-        for row in range(self.vertices):
-            print(self.mapping.get(row), ' ', end ='')
-        print()
-
-        for row in range(self.vertices):
-            print(self.mapping.get(row), ' ', end = '')
-            for column in range(self.vertices):
-                print(self.graph[row][column], '\t', end='')
-            print()
-
-        # for row in range(self.vertices):
-        #     output_result_list.append(str(self.edges[row]))
-
-        # write to output file
-        self.utils.writeToOutputFile(output_result_list)"""
-
     """
     since we have the list of interpreters, now its easy to distinguish, hence we iterate for all the vertices
     and whichever item is not in interpreter, we know its a language
